e0284ee2f23d560779ccbc6054351de57340ab15
HtetMonLinn <htetmon4512@gmail.com>
2021-09-07 15:49:24 +0630

    login system

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,23 +3,34 @@
   <form @submit.prevent="logIn">
       <input type="text" placeholder="email" v-model="email">
       <input type="text" placeholder="password" v-model="password"> <br> 
+      <div class="error" v-if="error">
+          {{error}}
+      </div>
       <button>Login</button>
   </form>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
+import {auth} from "../firebase/config"
+import useLogin from "../composables/useLogin"
+
 export default {
     setup(){
         
         let email=ref("");
         let password=ref("");
-        let logIn=()=>{
-
+        
+        let {error,signIn}=useLogin();
+        let logIn=async()=>{
+         let res=await signIn(email.value,password.value);
+         if(res){
+             console.log(res.user);
+         }
         }
 
 
-        return{email,password,logIn}
+        return{email,password,logIn,error}
     }
 
 }
